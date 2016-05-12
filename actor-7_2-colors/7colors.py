f10b8b547fd0a04f09be5c083efbe84b773ea0c3
dani <ni.cavalheiro@gmail.com>
2016-05-12 17:21:45 -0300

    Update 7colors.py

--- actor-7_2-colors/7colors.py
+++ actor-7_2-colors/7colors.py
@@ -1,29 +1,25 @@
 # Benoetigte Module werden importiert und eingerichtet
 import RPi.GPIO as GPIO
 import time
-   
-GPIO.setmode(GPIO.BCM)
-   
-# Hier werden die Ausgangs-Pin deklariert, an dem die LEDs angeschlossen sind.
-LED_ROT = 5
-LED_GRUEN = 4
-GPIO.setup(LED_ROT, GPIO.OUT, initial= GPIO.LOW)
-GPIO.setup(LED_GRUEN, GPIO.OUT, initial= GPIO.LOW)
-   
-print "LED-Test [druecken Sie STRG+C, um den Test zu beenden]"
   
+GPIO.setmode(GPIO.BCM)
+  
+# Hier wird der Eingangs-Pin deklariert, an dem der Sensor angeschlossen ist. Zusaetzlich wird auch der PullUP Widerstand am Eingang aktiviert
+LED_PIN = 24
+GPIO.setup(LED_PIN, GPIO.OUT, initial= GPIO.LOW)
+  
+print "LED-Test [druecken Sie STRG+C, um den Test zu beenden]"
+ 
 # Hauptprogrammschleife
 try:
         while True:
-            print("LED ROT 3 Sekunden an")
-            GPIO.output(LED_ROT,GPIO.HIGH) #LED wird eingeschaltet
-            GPIO.output(LED_GRUEN,GPIO.LOW) #LED wird eingeschaltet
-            time.sleep(3) # Wartemodus fuer 4 Sekunden
-            print("LED GRUEN 3 Sekunden an") 
-            GPIO.output(LED_ROT,GPIO.LOW) #LED wird eingeschaltet
-            GPIO.output(LED_GRUEN,GPIO.HIGH) #LED wird eingeschaltet
-            time.sleep(3) #Wartemodus fuer weitere zwei Sekunden, in denen die LED Dann ausgeschaltet ist
-   
+                print("LED 4 Sekunden an")
+                GPIO.output(LED_PIN,GPIO.HIGH) #LED wird eingeschaltet
+                time.sleep(4) #Wartemodus fuer 4 Sekunden
+                print("LED 2 Sekunden aus") 
+                GPIO.output(LED_PIN,GPIO.LOW) #LED wird ausgeschaltet
+                time.sleep(2) #Wartemodus fuer weitere zwei Sekunden, in denen die LED Dann ausgeschaltet ist
+  
 # Aufraeumarbeiten nachdem das Programm beendet wurde
 except KeyboardInterrupt:
         GPIO.cleanup()
