1e45ff0c1a93794e97c6275fc63132313f1b69bc
dani <ni.cavalheiro@gmail.com>
2016-05-12 17:20:46 -0300

    Update 7colors.py

--- actor-7_2-colors/7colors.py
+++ actor-7_2-colors/7colors.py
@@ -1,20 +1,29 @@
+# Benoetigte Module werden importiert und eingerichtet
 import RPi.GPIO as GPIO
-
+import time
+   
 GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(14, GPIO.OUT) #red
-GPIO.output(14,1)
-GPIO.setup(13, GPIO.OUT) #green
-GPIO.output(13,1)
-GPIO.setup(12, GPIO.OUT) #blue
-GPIO.output(12,1)
-
+   
+# Hier werden die Ausgangs-Pin deklariert, an dem die LEDs angeschlossen sind.
+LED_ROT = 5
+LED_GRUEN = 4
+GPIO.setup(LED_ROT, GPIO.OUT, initial= GPIO.LOW)
+GPIO.setup(LED_GRUEN, GPIO.OUT, initial= GPIO.LOW)
+   
+print "LED-Test [druecken Sie STRG+C, um den Test zu beenden]"
+  
+# Hauptprogrammschleife
 try:
-    while(True):
-        request = raw_input("RGB-->")
-        if (len(request) == 3):
-            GPIO.output(14, int(request[0]))
-            GPIO.output(13, int(request[1]))
-            GPIO.output(12, int(request[2]))
+        while True:
+            print("LED ROT 3 Sekunden an")
+            GPIO.output(LED_ROT,GPIO.HIGH) #LED wird eingeschaltet
+            GPIO.output(LED_GRUEN,GPIO.LOW) #LED wird eingeschaltet
+            time.sleep(3) # Wartemodus fuer 4 Sekunden
+            print("LED GRUEN 3 Sekunden an") 
+            GPIO.output(LED_ROT,GPIO.LOW) #LED wird eingeschaltet
+            GPIO.output(LED_GRUEN,GPIO.HIGH) #LED wird eingeschaltet
+            time.sleep(3) #Wartemodus fuer weitere zwei Sekunden, in denen die LED Dann ausgeschaltet ist
+   
+# Aufraeumarbeiten nachdem das Programm beendet wurde
 except KeyboardInterrupt:
-    GPIO.cleanup()
+        GPIO.cleanup()
