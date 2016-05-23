6d6b9f4088bd2e11b1d311a887d244d94ed8eabb
Rafael Koch Peres <Rafael Koch Peres>
2016-05-23 19:08:11 +0200

    Fix on buzzer.

--- example-applications/alarm_project/buzzer.py
+++ example-applications/alarm_project/buzzer.py
@@ -5,7 +5,7 @@ import spidev
 
 
 class ActorBuzzer ():
-    def __init__(pin=27, frequency=50, duration=10):
+    def __init__(self, pin=27, frequency=50, duration=10):
         self.pin = pin
         self.frequency = frequency
         self.duration = duration
@@ -14,7 +14,7 @@ class ActorBuzzer ():
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-    def buzz():
+    def buzz(self):
         p = GPIO.PWM(self.pin, self.frequency)
         p.start(50)
         time.sleep(float(self.duration)/1000)
